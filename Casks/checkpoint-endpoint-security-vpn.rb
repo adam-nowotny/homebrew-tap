abb04610ae301eff8542a8bc319d30016eff2b7b
Adam Nowotny <adam@nowotny.pl>
2025-04-29 14:46:11 +0200

    Update checkpoint-endpoint-security-vpn.rb
    up to E98.10

--- Casks/checkpoint-endpoint-security-vpn.rb
+++ Casks/checkpoint-endpoint-security-vpn.rb
@@ -1,8 +1,8 @@
 cask "checkpoint-endpoint-security-vpn" do
-  version "E89.02"
+  version "E89.10"
   sha256 "f9312dfe6af66acd73cc639eb7e4bd06ce0953fa8d63bb6dae2c5de21a60d280"
 
-  url "https://dl3.checkpoint.com/paid/02/02b9348c633849369b4d363c1c359171/Endpoint_Security_VPN.pkg?HashKey=1745322143_977747014a1f9cbd94063ef0f93db8d3&xtn=.pkg"
+  url "https://dl3.checkpoint.com/paid/06/0688f4c7999b46465364031c1017accc/Endpoint_Security_VPN.pkg?HashKey=1745937859_77c167666484576baa900592cb119917&xtn=.pkg"
   name "Checkpoint Endpoint Security VPN"
   desc "VPN Checkpoint Endpoint Security VPN"
   homepage "https://support.checkpoint.com/results/download/136624"
