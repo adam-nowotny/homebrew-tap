57cf4982602c20ed32370311727a6492715a2e61
Adam Nowotny <adam@nowotny.pl>
2025-04-29 14:47:30 +0200

    Update checkpoint-endpoint-security-vpn.rb
    up sha256 and url

--- Casks/checkpoint-endpoint-security-vpn.rb
+++ Casks/checkpoint-endpoint-security-vpn.rb
@@ -1,11 +1,11 @@
 cask "checkpoint-endpoint-security-vpn" do
   version "E89.10"
-  sha256 "f9312dfe6af66acd73cc639eb7e4bd06ce0953fa8d63bb6dae2c5de21a60d280"
+  sha256 "e3b0faeeb84d8df7108313f2d4bb400f45748fec8dbcdee8e490c7f951fc23b5"
 
   url "https://dl3.checkpoint.com/paid/06/0688f4c7999b46465364031c1017accc/Endpoint_Security_VPN.pkg?HashKey=1745937859_77c167666484576baa900592cb119917&xtn=.pkg"
   name "Checkpoint Endpoint Security VPN"
   desc "VPN Checkpoint Endpoint Security VPN"
-  homepage "https://support.checkpoint.com/results/download/136624"
+  homepage "https://support.checkpoint.com/results/download/136510"
 
   pkg "Endpoint_Security_VPN.pkg"
 
